5af563402f2cfb90b1acf66ff9e618d289213caf
lukaszdejko <lukasz.f.d@gmail.com>
2026-03-18 11:08:28 +0100

    update

---
+++ AI for Blue Team/BeautifulSoup.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+
+html_doc = """ <html>
+<head><title>Birbs are pretty</title></head>
+<body><p class="birb-food"><b>Birbs and their foods</b></p>
+<p class="food">Birbs love:<a class="seed" href="http://seeds" id="seed">seed</a>
+   and 
+   <a class="fruit" href="http://fruit" id="fruit">fruit</a></p>
+ </body></html> """
+soup = BeautifulSoup(html_doc, 'html.parser')
+print(soup.prettify())
